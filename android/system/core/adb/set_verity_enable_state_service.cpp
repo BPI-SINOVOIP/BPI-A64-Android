45d29c64a650b51494af2c1a6ce140708c73c9d0
August <mingxin.android@gmail.com>
2016-09-27 19:42:34 +0800

    set the right dm-verity device path for adb disable-verity

--- android/system/core/adb/set_verity_enable_state_service.cpp
+++ android/system/core/adb/set_verity_enable_state_service.cpp
@@ -41,6 +41,8 @@ static const bool kAllowDisableVerity = true;
 static const bool kAllowDisableVerity = false;
 #endif
 
+#define VERITY_BLOCK         "/dev/block/by-name/verity_block"
+
 static int get_target_device_size(int fd, const char *blk_device,
                                   uint64_t *device_size)
 {
@@ -75,6 +77,7 @@ static int get_target_device_size(int fd, const char *blk_device,
     return 0;
 }
 
+#if 0  /*bpi*/
 /* Turn verity on/off */
 static int set_verity_enabled_state(int fd, const char *block_device,
                                     const char* mount_point, bool enable)
@@ -152,6 +155,87 @@ errout:
     return retval;
 }
 
+#else
+
+/* Turn verity on/off */
+static int set_verity_enabled_state(int fd, const char *block_device,
+                                    bool enable)
+{
+    uint32_t magic_number;
+    const uint32_t new_magic = enable ? VERITY_METADATA_MAGIC_NUMBER
+                                      : VERITY_METADATA_MAGIC_DISABLE;
+    uint64_t device_length = 0;
+    int device = -1;
+    int retval = -1;
+
+    if (!make_block_device_writable(block_device)) {
+        WriteFdFmt(fd, "Could not make block device %s writable (%s).\n",
+                   block_device, strerror(errno));
+        goto errout;
+    }
+
+    device = adb_open(block_device, O_RDWR | O_CLOEXEC);
+    if (device == -1) {
+        WriteFdFmt(fd, "Could not open block device %s (%s).\n", block_device, strerror(errno));
+        WriteFdFmt(fd, "Maybe run adb remount?\n");
+        goto errout;
+    }
+
+    // find the start of the verity metadata
+    if (get_target_device_size(fd, (char*)block_device, &device_length) < 0) {
+        WriteFdFmt(fd, "Could not get target device size.\n");
+        goto errout;
+    }
+
+    if (lseek64(device, device_length, SEEK_SET) < 0) {
+        WriteFdFmt(fd, "Could not seek to start of verity metadata block.\n");
+        goto errout;
+    }
+
+    // check the magic number
+    if (adb_read(device, &magic_number, sizeof(magic_number)) != sizeof(magic_number)) {
+        WriteFdFmt(fd, "Couldn't read magic number!\n");
+        goto errout;
+    }
+
+    if (!enable && magic_number == VERITY_METADATA_MAGIC_DISABLE) {
+        WriteFdFmt(fd, "Verity already disabled\n");
+        goto errout;
+    }
+
+    if (enable && magic_number == VERITY_METADATA_MAGIC_NUMBER) {
+        WriteFdFmt(fd, "Verity already enabled\n");
+        goto errout;
+    }
+
+    if (magic_number != VERITY_METADATA_MAGIC_NUMBER
+            && magic_number != VERITY_METADATA_MAGIC_DISABLE) {
+        WriteFdFmt(fd, "Couldn't find verity metadata at offset %" PRIu64 "!\n", device_length);
+        goto errout;
+    }
+
+    if (lseek64(device, device_length, SEEK_SET) < 0) {
+        WriteFdFmt(fd, "Could not seek to start of verity metadata block.\n");
+        goto errout;
+    }
+
+    if (adb_write(device, &new_magic, sizeof(new_magic)) != sizeof(new_magic)) {
+        WriteFdFmt(fd, "Could not set verity %s flag on device %s with error %s\n",
+                   enable ? "enabled" : "disabled",
+                   block_device, strerror(errno));
+        goto errout;
+    }
+
+    WriteFdFmt(fd, "Verity %s\n", enable ? "enabled" : "disabled");
+    retval = 0;
+errout:
+    if (device != -1)
+        adb_close(device);
+    return retval;
+}
+
+#endif /*end bpi*/
+
 void set_verity_enabled_state_service(int fd, void* cookie)
 {
     bool enable = (cookie != NULL);
@@ -186,9 +270,14 @@ void set_verity_enabled_state_service(int fd, void* cookie)
         /* Loop through entries looking for ones that vold manages */
         for (i = 0; i < fstab->num_entries; i++) {
             if(fs_mgr_is_verified(&fstab->recs[i])) {
+#if 0 /*bpi*/
                 if (!set_verity_enabled_state(fd, fstab->recs[i].blk_device,
                                               fstab->recs[i].mount_point,
                                               enable)) {
+#else
+		if (!set_verity_enabled_state(fd, VERITY_BLOCK,
+                                              enable)) {
+#endif
                     any_changed = true;
                 }
            }
